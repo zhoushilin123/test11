88f2687035c4f4ac96b1281965bd95b33bf00314
zhoushilin <2813447427@qq.com>
2021-07-28 08:37:07 +0800

    第一次提交

---
+++ src/com/atzhou/java/test1.java
@@ -0,0 +1,12 @@
+package com.atzhou.java;
+
+/**
+ * @author shkstart
+ * @create 2021-07-28 8:34
+ */
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
